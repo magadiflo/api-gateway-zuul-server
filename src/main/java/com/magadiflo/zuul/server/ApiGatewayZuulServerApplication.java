4d3f0c07c2f62e7944942a12a42cd2c7dede763d
Martín <magadiflo@gmail.com>
2022-11-03 20:02:08 -0500

    Habilitando proyecto como proyecto de Zuul y cliente de Eureka

--- src/main/java/com/magadiflo/zuul/server/ApiGatewayZuulServerApplication.java
+++ src/main/java/com/magadiflo/zuul/server/ApiGatewayZuulServerApplication.java
@@ -2,7 +2,11 @@ package com.magadiflo.zuul.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient // Para definir este proyecto como cliente de Eureka
+@EnableZuulProxy // Habilitamos Zuul en el proyecto
 @SpringBootApplication
 public class ApiGatewayZuulServerApplication {
 
