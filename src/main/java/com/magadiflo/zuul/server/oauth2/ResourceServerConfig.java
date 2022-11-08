8d35ec0ebc2efaa967f7430c3f510975391633ae
Martín <magadiflo@gmail.com>
2022-11-08 13:53:23 -0500

    Obteniendo clave de firma del JWT desde el servidor de configuraciones

--- src/main/java/com/magadiflo/zuul/server/oauth2/ResourceServerConfig.java
+++ src/main/java/com/magadiflo/zuul/server/oauth2/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.magadiflo.zuul.server.oauth2;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,9 +12,20 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+/**
+ * @RefreshScope, si queremos actualizar alguna configuración que se agregó en el 
+ * servidor de configuraciones y queremos tenerlo en esta aplicación, 
+ * pero no queremos reiniciar (esta aplicación) usamos el actuator (mediante refresh)
+ *
+ */
+@RefreshScope
 @EnableResourceServer // Habilitamos la configuración del servidor de recurso
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	// Como es un solo valor que usaremos, lo obtenemos con el Value. En el microservicio Oauth2 usamos el Environment
+	@Value("${config.security.oauth.jwt.key}")
+	private String jwtKey;
 
 	// Para el token
 	@Override
@@ -42,7 +55,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() { // Información, datos por defecto del JWT
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("clave12345");				
+		tokenConverter.setSigningKey(this.jwtKey);				
 		return tokenConverter;
 	}
 
